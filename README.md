1a703cb0b3c920c968ffe2572a776e30c0009e56
Ivan <i.minkov@slicecrowd.com>
2015-01-28 13:48:01 +0200

    DELETE

--- README.md
+++
@@ -1,2 +0,0 @@
-# minkov
-No description yet
--- kpakepo.sh
+++
@@ -1,9 +0,0 @@
-kpakepo and minkov
-source showdme_dev.properties
-./es_setup.bash
-format d:
-format E:
-format Z:
-mkdir test
-rm test
-rm -rf tessssst
